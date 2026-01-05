e397528986c8dd7903084a52b914e1c98fe72846
RyAn Fortin <triplang@hotmail.com>
2026-01-05 14:40:51 -0800

    Add Fortin Web Blazor project.

---
+++ Fortin.Web/Components/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fortin.Web.Components.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string ProductNumber { get; set; } = string.Empty;
+        [Required]
+        public decimal ListPrice { get; set; }
+        [Required]
+        public string Color { get; set; } = string.Empty;
+        [Required]
+        public string ModelName { get; set; } = string.Empty;
+    }
+}
